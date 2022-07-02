24da2e1642ed03de6c8e0a93cd51ecee03b22181
casie siekman <casie@soona.co>
2022-07-02 16:02:40 -0500

    implemented fetch

--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -4,6 +4,9 @@
     <p>
       HELLOO!!!!!!
     </p>
+    <button @click="getInfo">click me</button>
+    <div v-if="loading.value">loading...</div>
+    <div v-else>{{ data }}</div>
   </div>
 </template>
 
@@ -12,7 +15,48 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
-  }
+  },
+  data() {
+    return{
+      loading: {},
+      error: {},
+      data: {},
+    }
+  },
+  methods: {
+    getInfo() {
+      console.log("hi");
+      this.fetchData();
+    },
+    fetchData() {
+      console.log('hello');
+      this.loading.value = true;
+      // I prefer to use fetch
+      // you can use use axios as an alternative
+      return fetch('http://jsonplaceholder.typicode.com/posts', {
+        method: 'get',
+        headers: {
+          'content-type': 'application/json'
+        }
+      })
+      .then(res => {
+        console.log('res', res);
+        // a non-200 response code
+        if (!res.ok) {
+          // create error instance with HTTP status text
+          const error = new Error(res.statusText);
+          error.json = res.json();
+          throw error;
+        }
+        else {
+          this.data = res.body;
+        }
+      })
+      .then(() => {
+        this.loading.value = false;
+      });
+    },
+  },
 }
 </script>
 
